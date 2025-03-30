1016ad1915642cf65117192327f4f5f44d543414
001227297 <Roberto.KUFFEL@tafesa.edu.au>
2025-03-31 10:26:42 +1030

    Add mortgage calculator code

--- GitCalculator/Controls/NumberButtonControl.xaml.cs
+++ GitCalculator/Controls/NumberButtonControl.xaml.cs
@@ -1,10 +1,16 @@
-﻿using Windows.UI.Xaml;
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Calculator.Controls
 {
 	public sealed partial class NumberButtonControl : UserControl
 	{
+		public NumberButtonControl()
+		{
+			this.InitializeComponent();
+		}
 		// dependencies
 		public string Value
 		{
@@ -15,13 +21,11 @@ namespace Calculator.Controls
 		public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string), typeof(NumberButtonControl), null);
 
 		// default functions
-		public NumberButtonControl()
-		{
-			InitializeComponent();
-		}
+		
 
 		// handle functions
-		private void handleClick(object sender, RoutedEventArgs e) {
+		private void handleClick(object sender, RoutedEventArgs e)
+		{
 			Utils.Utils.handleClick((string)(sender as Button).Content);
 		}
 	}
